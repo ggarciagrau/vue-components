a37490ecd40c71bda7433f2e0b4d802e0fd65f99
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-10-14 17:27:57 +0200

    teleports

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,10 @@
 <script setup>
-import { ref } from 'vue';
-
-
-const textIsVissible = ref(true);
-
-const displayText = () => {
-  if (textIsVissible.value) {
-    textIsVissible.value = false
-    return
-  }
-
-  textIsVissible.value = true
-}
+import TeleportedModal from './components/TeleportedModal.vue';
 </script>
 
 <template>
-  <button @click="displayText">Text</button>
-  <transition name="bounce">
-    <p v-show="textIsVissible" style="text-align: center;">Hello world</p>
-  </transition>
+  <TeleportedModal />
 </template>
 
 <style scoped>
-
-.bounce-enter-active {
-  animation: bounce 1s;
-}
-
-.bounce-leave-active {
-  animation: bounce 1s reverse;
-}
-
-@keyframes bounce {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.5);
-    color: red;
-  }
-  100% {
-    transform: scale(1);
-  }
-}
 </style>
---
+++ src/components/TeleportedModal.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const modalIsVissible = ref(false);
+
+const toggle = () => modalIsVissible.value = !modalIsVissible.value;
+
+</script>
+
+<template>
+    <div>
+        <button v-if="!modalIsVissible" @click="toggle">Open modal</button>
+        <Teleport to="#modalWrapper">
+            <div v-show="modalIsVissible" class="modal">
+                <h1>Modal</h1>
+                <p>Modal content</p>
+                <button @click="toggle">Close</button>
+            </div>
+        </Teleport>
+    </div>
+</template>
+
+<style scoped>
+.modal {
+    border-collapse: collapse;
+    border: 5px solid black;
+    width: 30%;
+}
+</style>
