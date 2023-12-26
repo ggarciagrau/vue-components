620463873a7f54a3f8f31392ce16815468ff6abd
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-12-27 00:09:18 +0100

    Composition API lifecycle

--- src/components/FirstCompositionComponent.vue
+++ src/components/FirstCompositionComponent.vue
@@ -1,11 +1,26 @@
 <script>
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, onMounted, onBeforeUnmount, onUnmounted, onBeforeUpdate, onUpdated } from 'vue';
 
 export default {
     // beforeCreated and created lifecycle events are replaced by setup function
     setup() {
         onBeforeMount(function () {
             console.log('On beofre mount')
+        })
+        onMounted(function () {
+            console.log('Mounted')
+        }),
+        onBeforeUnmount(function() {
+            console.log('On before umount')
+        }),
+        onUnmounted(function () {
+            console.log('Unmounted')
+        }),
+        onBeforeUpdate(function() {
+            console.log('On before update')
+        }),
+        onUpdated(function () {
+            console.log('Updated')
         })
     }
 }
